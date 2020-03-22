44c265112056dc2bca53517a9ba295ab27329cf7
Benny Ho <benny.ho2530@gmail.com>
2020-03-22 18:16:22 +0100

    added constructor and event listeners

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-/*
+
 class UI {
   constructor() {
     this.budgetFeedback = document.querySelector(".budget-feedback");
@@ -17,9 +17,50 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
-}
-*/
 
+}
+
+function eventListeners() {
+
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  // new instance of UI CLASS
+
+  const ui = new UI();
+
+  // budget form submit
+
+  budgetForm.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+  })
+
+
+  // expense form submit
+
+  expenseForm.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+  })
+
+
+  // expense click
+
+  expenseList.addEventListener('click', function(event) {
+
+
+  })
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  eventListeners();
+})
+
+
+/*
 // event listener
 
 function eventListener() {
@@ -62,7 +103,52 @@ function eventListener() {
 
     }
   })
+
+  expenseForm.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    const expenseText = expenseInput.value;
+    const amountInputExpense = amountInput.value;
+    const budget = budgetAmount.textContent;
+
+
+
+    if(expenseText === '' || amountInputExpense === '') {
+      expenseFeedback.classList.add('showItem');
+      expenseFeedback.textContent = `values cannot be empty or negative`;
+
+      setTimeout(() => {
+        expenseFeedback.classList.remove('showItem');
+      }, 4000);
+    } else {
+
+      const expense = new Expense(expenseText, amountInputExpense);
+
+
+      expense.clearFields(expenseInput, amountInput);
+      expense.addExpense(expenseList,  expense, expenseAmount);
+      expense.calculation(expense,budget, balance);
+
+    
+    }
+  });
+
+  expenseList.addEventListener('click', function(event) {
+
+    event.preventDefault();
+
+    console.log(event.target.parentElement.parentElement.parentElement.parentElement);
+
+    if(event.target.parentElement.classList.contains('delete-icon')) {
+
+      expenseList.removeChild(event.target.parentElement.parentElement.parentElement.parentElement);
+    }
+
+    // data needs to be added
+  })
 }
+
 
 // ui constructor
 
@@ -88,9 +174,70 @@ Budget.prototype.addBudget = function(budgetValue, budgetAmount, balance) {
   balance.textContent = budgetValue;
 }
 
+
+// expense constructor
+
+function Expense(title, amount) {
+
+  this.title = title;
+  this.amount = amount;
+}
+
+Expense.prototype.clearFields = function(expenseInput, amountInput) {
+
+  expenseInput.value = '';
+  amountInput.value = '';
+}
+
+Expense.prototype.addExpense = function(element, expense, expenseAmount) {
+
+  const div = document.createElement('div');
+  div.classList.add('expense');
+  div.innerHTML = `
+  <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+  `
+  
+  expenseAmount.textContent = expense.amount
+
+  element.appendChild(div);
+
+ 
+}
+
+// delete Expense
+Expense.prototype.deleteExpense = function() {
+
+}
+
+Expense.prototype.calculation = function(expense, budget, balance) {
+
+  const totalBalance = budget - expense.amount;
+  console.log(totalBalance);
+
+  balance.textContent = totalBalance;
+
+
+}
+
+
 // dom event listener
 
 document.addEventListener('DOMContentLoaded', function() {
 
   eventListener();
 })
+*/
